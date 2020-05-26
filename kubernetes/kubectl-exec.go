c0079252d750b6314a647be87d9918142606010c
caoyuan <cao.yingjunz@gmail.com>
2020-05-26 23:56:56 +0800

    Add kubectl exec

---
+++ kubernetes/kubectl-exec.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// https://itnext.io/how-it-works-kubectl-exec-e31325daa910
+// https://erkanerol.github.io/post/how-kubectl-exec-works/
+// 分析 kubectl exec 的工作原理
+
+func main() {
+	fmt.Print("分析 kubectl exec 工作原理")
+}
--- kubernetes/kubectl.go
+++ kubernetes/kubectl.go
@@ -2,10 +2,6 @@ package main
 
 import "fmt"
 
-// https://itnext.io/how-it-works-kubectl-exec-e31325daa910
-// https://erkanerol.github.io/post/how-kubectl-exec-works/
-// 分析 kubectl exec 的工作原理
-
 // 学习实践 kubectl 的 plugin https://github.com/kubernetes/sample-cli-plugin
 // https://github.com/kubernetes/kubernetes/tree/master/staging/src/k8s.io/sample-cli-plugin
 
